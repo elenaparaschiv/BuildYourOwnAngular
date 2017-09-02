e60376afa0f2ae67bf6b6881c437d68cbb35817b
Elena <bri_elena@yahoo.com>
2017-09-02 13:28:09 +0300

    finished ch. Keeping The Digest Going While It Stays Dirty

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -104,6 +104,37 @@ describe('Scope', function() {
     });
 
 
+    it('triggers chained watchers in the same digest', function() {
+      scope.name = 'Jane';
+
+      scope.$watch(
+        function(scope) { return scope.nameUpper; },
+        function(newValue, oldValue, scope) {
+          if(newValue) {
+            scope.initial = newValue.substring(0,1) + '.';
+          }
+        }
+      );
+
+      scope.$watch(
+        function(scope) { return scope.name; },
+        function(newValue, oldValue, scope) {
+          if(newValue) {
+            scope.nameUpper = newValue.toUpperCase();
+          }
+        }
+      );
+
+      scope.$digest();
+      expect(scope.initial).toBe('J.');
+
+      scope.name = 'Bob';
+      scope.$digest();
+      expect(scope.initial).toBe('B.');
+
+    });
+
+
 
   });
 });
